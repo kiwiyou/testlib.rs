f632161606979e31de1181ac415882b0bbc6334d
kiwiyou <kiwiyou@kiwiyou.dev>
2023-03-04 03:02:59 +0900

    Add validator

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,176 @@ use std::{
     collections::hash_map::DefaultHasher,
     fmt::Debug,
     hash::{Hash, Hasher},
+    io::Read,
     ops::{Bound, RangeBounds},
     str::FromStr,
 };
+
+const BUF: usize = 1 << 17;
+/// Validator for [`std::io::Read`].
+pub struct Validator<R> {
+    buf: Vec<u8>,
+    front: usize,
+    reader: R,
+    line: usize,
+}
+
+impl<R: Read> Validator<R> {
+    /// Creates a new validator wrapping `reader`.
+    pub fn new(reader: R) -> Self {
+        Self {
+            buf: vec![0; BUF],
+            front: BUF,
+            reader,
+            line: 1,
+        }
+    }
+    /// Reads a byte without advancing cursor.
+    fn read_byte(&mut self) -> Option<u8> {
+        let ret = if self.front < self.buf.len() {
+            self.buf[self.front]
+        } else {
+            let capacity = self.buf.capacity();
+            self.buf.resize(capacity, 0);
+            let len = self
+                .reader
+                .read(&mut self.buf)
+                .expect("cannot read from reader");
+            if len == 0 {
+                return None;
+            }
+            self.buf.truncate(len);
+            self.front = 0;
+            self.buf[self.front]
+        };
+        Some(ret)
+    }
+    /// Reads an integer (signed/unsigned) with bound check, panics if not found or out of bound.
+    pub fn read_int<
+        Int: FromStr + PartialOrd,
+        Range: Borrow<Bounds>,
+        Bounds: RangeBounds<Int> + Debug,
+    >(
+        &mut self,
+        range: Range,
+        name: &str,
+    ) -> Int {
+        let range = range.borrow();
+        let mut s = vec![];
+        loop {
+            let digit = self.read_byte();
+            match digit {
+                Some(digit @ b'-' | digit @ b'0'..=b'9') => {
+                    s.push(digit);
+                    self.front += 1;
+                }
+                _ => break,
+            }
+        }
+        if s.is_empty() {
+            panic!("{name}: expected integer, found EOF");
+        }
+        // Safety guarantee: s only contains b'-' and b'0'..=b'9'.
+        let s = unsafe { String::from_utf8_unchecked(s) };
+        match s.parse() {
+            Ok(v) if range.contains(&v) => v,
+            _ => panic!("{name}: expected integer in bound {range:?}, found {s:?}"),
+        }
+    }
+    /// Reads a space, panics if not found.
+    pub fn read_space(&mut self) {
+        let ch = self.read_byte();
+        if ch != Some(b' ') {
+            panic!(
+                "[LINE #{}] expected space, found {:?}",
+                self.line,
+                ch.map(|b| b as char)
+            );
+        }
+        self.front += 1;
+    }
+    /// Reads a token, panics if not found or invalid utf-8.
+    pub fn read_token(&mut self, name: &str) -> String {
+        let mut s = vec![];
+        while let Some(ch) = self.read_byte() {
+            if ch.is_ascii_whitespace() {
+                break;
+            }
+            s.push(ch);
+        }
+        if s.is_empty() {
+            panic!("{name}: expected string, found EOF");
+        }
+        match String::from_utf8(s) {
+            Ok(s) => s,
+            Err(_) => panic!("{name}: invalid utf-8 string"),
+        }
+    }
+    /// Reads a line, panics if not found or invalid utf-8.
+    pub fn read_line(&mut self, name: &str) -> String {
+        let mut s = vec![];
+        while let Some(ch) = self.read_byte() {
+            if ch == b'\r' || ch == b'\n' {
+                break;
+            }
+            s.push(ch);
+        }
+        if s.is_empty() {
+            panic!("{name}: expected string, found EOF");
+        }
+        match String::from_utf8(s) {
+            Ok(s) => s,
+            Err(_) => panic!("{name}: invalid utf-8 string"),
+        }
+    }
+    /// Checks for newline `"\n"` ad `"\r\n"`, panics if not found.
+    pub fn read_eoln(&mut self) {
+        match self.read_byte() {
+            Some(b'\r') => {
+                self.front += 1;
+                if let Some(b'\n') = self.read_byte() {
+                    self.front += 1;
+                    self.line += 1;
+                } else {
+                    panic!("[LINE #{}] expected EOLN, found {:?}", self.line, '\r');
+                }
+            }
+            Some(b'\n') => {
+                self.front += 1;
+                self.line += 1;
+            }
+            None => {
+                panic!("[LINE #{}] expected EOLN, found EOF", self.line);
+            }
+            Some(other) => {
+                panic!(
+                    "[LINE #{}] expected EOLN, found {:?}",
+                    self.line, other as char
+                );
+            }
+        }
+    }
+    /// Checks for EOF, panics if not found.
+    pub fn read_eof(&mut self) {
+        if let Some(ch) = self.read_byte() {
+            panic!("[LINE #{}] expected EOF, found {:?}", self.line, ch as char);
+        }
+    }
+    /// Skip whitespaces and check if it reached EOF.
+    pub fn seek_eof(&mut self) -> bool {
+        loop {
+            match self.read_byte() {
+                Some(b'\n') => {
+                    self.front += 1;
+                    self.line += 1;
+                }
+                Some(ch) if ch.is_ascii_whitespace() => self.front += 1,
+                Some(_) => break false,
+                None => break true,
+            }
+        }
+    }
+}
 
 #[cfg(windows)]
 pub const EOLN: &str = "\r\n";
@@ -50,7 +217,7 @@ impl Rng {
             prev
         }))
     }
-    /// Generates a random `u64`.
+    /// Generates a random [`u64`].
     pub fn next_u64(&mut self) -> u64 {
         let [x, y, z, c] = &mut self.0;
         let t = x.wrapping_shl(58).wrapping_add(*c);
@@ -65,7 +232,7 @@ impl Rng {
         *y ^= y.wrapping_shl(43);
         x.wrapping_add(*y).wrapping_add(*z)
     }
-    /// Generates a random `u64` ranges `range`.
+    /// Generates a random [`u64`] in `range`.
     ///
     /// Panics if no number is present in the range.
     ///
@@ -77,7 +244,10 @@ impl Rng {
     /// let my_number = rng.next_range(12..13);
     /// assert!((12..13).contains(&my_number));
     /// ```
-    pub fn next_range<R: Borrow<B>, B: RangeBounds<u64> + Debug>(&mut self, range: R) -> u64 {
+    pub fn next_range<Range: Borrow<Bounds>, Bounds: RangeBounds<u64> + Debug>(
+        &mut self,
+        range: Range,
+    ) -> u64 {
         let range = range.borrow();
         let min = match range.start_bound() {
             Bound::Included(&min) => min,
@@ -98,17 +268,17 @@ impl Rng {
         let next = (self.next_u64() as u128 * len as u128) >> 64;
         min + next as u64
     }
-    /// Generates a random `f64` in interval `[0, 1)`.
+    /// Generates a random [`f64`] in interval `[0, 1)`.
     pub fn next_f64(&mut self) -> f64 {
         self.next_u64() as f64 * 5.42101086242752217003726400434970e-20
     }
-    /// Generates a weighted random `u64` in range `range`, with weight `w`.
+    /// Generates a weighted random [`u64`] in range `range`, with weight `w`.
     ///
     /// Positive `w` means maximum of `w`+1 random values.
     /// Negative `w` means minimum of `w`+1 random values.
-    pub fn wnext_range<R: Borrow<B>, B: RangeBounds<u64> + Debug>(
+    pub fn wnext_range<Range: Borrow<Bounds>, Bounds: RangeBounds<u64> + Debug>(
         &mut self,
-        range: R,
+        range: Range,
         w: i32,
     ) -> u64 {
         let range = range.borrow();
@@ -128,14 +298,14 @@ impl Rng {
             panic!("No matching integers in bound {range:?}");
         }
         if w.abs() < 25 {
-            let mut r = self.next_range::<&B, B>(range);
+            let mut r = self.next_range::<&Bounds, Bounds>(range);
             if w < 0 {
                 for _ in 0..-w {
-                    r = r.min(self.next_range::<&B, B>(range));
+                    r = r.min(self.next_range::<&Bounds, Bounds>(range));
                 }
             } else {
                 for _ in 0..w {
-                    r = r.max(self.next_range::<&B, B>(range));
+                    r = r.max(self.next_range::<&Bounds, Bounds>(range));
                 }
             }
             r
